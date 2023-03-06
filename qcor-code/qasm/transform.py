8b319c9e49ee736dcdbae5e711a1229b8a20d84e
KJay221 <k20002216408@gmail.com>
2023-03-06 16:33:41 +0800

    remove trash file

--- qcor-code/qasm/transform.py
+++ qcor-code/qasm/transform.py
@@ -1,34 +1,75 @@
 import sys
+import math
 
 gate_list = {}
 gate_count = -4
 
-two_word = ["u3", "rx", "ry", "rz"]
+special = ["u3", "rx", "ry", "rz"]
 
-with open(sys.argv[1]) as file:
-    while (line := file.readline().rstrip()):
-        flag = 0
-        gate_count += 1
-        for gate in two_word:
-            if line.split( )[0][0:2] == gate:
-                flag = 1
-                if gate not in gate_list:
-                    gate_list[gate] = 1
-                else:
-                    gate_list[gate] += 1
-                break
-        if flag == 1:
-            continue
-        if line.split( )[0] not in gate_list:
-            gate_list[line.split( )[0]] = 1
-        else:   
-            gate_list[line.split( )[0]] += 1
+with open("output.txt", 'w') as out:
+    with open(sys.argv[1]) as file:
+        while (line := file.readline().rstrip()):
+            flag = 0
+            gate_count += 1
+            if gate_count <= 0:
+                continue
+            for gate in special:
+                if line.split(' ')[0][0:2] == gate:
+                    flag = 1
+                    if gate not in gate_list:
+                        gate_list[gate] = 1
+                    else:
+                        gate_list[gate] += 1
+                    break
+            if flag == 1:
+                ## output file
+                if line.split(' ')[0][0:2] == "rx":
+                    n_1 = line.split(' ')[0].split('(')[1].split(')')[0]
+                    real_1 = round(math.cos(float(n_1)/2), 8)
+                    imag_1 = round(-math.sin(float(n_1)/2), 8)
+                    out.write("U1(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] 
+                                    + ", [" + str(real_1) + ", 0, 0, " + str(real_1) + "]"
+                                    + ", [" + str(imag_1) + ", 0, 0, " + str(imag_1) + "])\n")
+                elif line.split(' ')[0][0:2] == "ry":
+                    n_1 = line.split(' ')[0].split('(')[1].split(')')[0]
+                    real_1 = round(math.cos(float(n_1)/2), 8)
+                    real_3 = round(math.sin(float(n_1)/2), 8)
+                    out.write("U1(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] 
+                                    + ", [" + str(real_1) + ", " + str(-real_3) + ", " + str(real_3) + ", " + str(real_1) + "]"
+                                    + ", [0, 0, 0, 0])\n")
+                elif line.split(' ')[0][0:2] == "rz":
+                    n_1 = line.split(' ')[0].split('(')[1].split(')')[0]
+                    real_1 = round(math.cos(float(n_1)/2), 8)
+                    imag_1 = round(-math.sin(float(n_1)/2), 8)
+                    out.write("U1(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] 
+                                    + ", [" + str(real_1) + ", 0, 0, " + str(real_1) + "]"
+                                    + ", [" + str(imag_1) + ", 0, 0, " + str(-imag_1) + "])\n")
+                continue
 
-gate_list.pop('OPENQASM', None)
-gate_list.pop('include', None)
-gate_list.pop('qreg', None)
-gate_list.pop('creg', None)
+
+            if line.split(' ')[0] not in gate_list:
+                gate_list[line.split(' ')[0]] = 1
+            else:   
+                gate_list[line.split(' ')[0]] += 1
+            ## output file
+            if line.split(' ')[0] == "CX":
+                out.write("CX(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] + ", " + line.split(' ')[2].split('[')[1].split(']')[0] + ")\n")
+            elif line.split(' ')[0] == "h":
+                out.write("H(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] + ")\n")
+            elif line.split(' ')[0] == "t":
+                out.write("T(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] + ")\n")
+            elif line.split(' ')[0] == "x":
+                out.write("X(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] + ")\n")
+            elif line.split(' ')[0] == "s":
+                out.write("S(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] + ")\n")
+            elif line.split(' ')[0] == "tdg":
+                out.write("U1(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] 
+                                + ", [1, 0, 0, 0.70710678]"
+                                + ", [0, 0, 0, -0.70710678])\n")
+            elif line.split(' ')[0] == "sdg":
+                out.write("U1(circuit, " + line.split(' ')[1].split('[')[1].split(']')[0] 
+                                + ", [1, 0, 0, 0]"
+                                + ", [0, 0, 0, -1])\n")
+
 print(gate_list)
 print("gate_count: " + str(gate_count))
-
-## rx ry rz u3
