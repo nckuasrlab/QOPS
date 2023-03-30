c03228be53ff58398dc8507464dc2816de653eb4
aben20807 <aben20807@gmail.com>
2023-03-30 10:32:50 +0800

    Add a wrap makefile to avoid changing original one

---
+++ llvm-pass-qpgo/simplifiedStateVector/src/pgo.mk
@@ -0,0 +1,24 @@
+CC:=~/.llvm/bin/clang
+MODE:=context
+FILE:=default.profdata
+SHARED_CFLAGS:=-O3 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64
+CLANG_PGO_CFLAGS:=$(SHARED_CFLAGS) -Qunused-arguments -Xclang -load -Xclang /home/nckucsieserver/pro/selfpro/llvm-pass-qpgo/build/qpgo/libQpgoPass.so -mllvm -profile-gen=$(FILE) -mllvm -profile-mode=$(MODE)
+
+all:
+	$(MAKE) -f makefile CC='$(CC)' CFLAGS='$(CLANG_PGO_CFLAGS)'
+
+clang:
+	$(MAKE) -f makefile CC='$(CC)' CFLAGS='$(SHARED_CFLAGS)'
+
+gcc:
+	$(MAKE) -f makefile CC='gcc' CFLAGS='$(SHARED_CFLAGS)'
+
+run:
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/nckucsieserver/.llvm/lib QPGO_PROFILE_FILE=$(FILE) ./qSim.out
+
+emit:
+	$(MAKE) -f makefile CC='$(CC)' CFLAGS='$(CLANG_PGO_CFLAGS) -S -emit-llvm'
+
+clean:
+	$(MAKE) -f makefile clean
+
